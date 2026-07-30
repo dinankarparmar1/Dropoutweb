997de7466258560bb419783931859a6540b665d3
dinankarparmar1 <dinankarparmar12345@gmail.com>
2026-07-30 11:02:19 +0530

    Create stocks.js

---
+++ css/js/stocks.js
@@ -0,0 +1,37 @@
+const stocks = {
+  TCS: {
+    name: "Tata Consultancy Services",
+    price: "₹3,540",
+    marketCap: "₹12.8 Lakh Cr",
+    pe: "30.2",
+    eps: "117.2",
+    roe: "48%",
+    debt: "0.08",
+    score: 90,
+    signal: "🟢 STRONG BUY",
+    health: 95,
+    growth: 92,
+    valuation: 80,
+    risk: 20,
+    profitability: 96,
+    cashflow: 94
+  },
+
+  RELIANCE: {
+    name: "Reliance Industries",
+    price: "₹3,025",
+    marketCap: "₹20.5 Lakh Cr",
+    pe: "28.6",
+    eps: "105.8",
+    roe: "9%",
+    debt: "0.42",
+    score: 84,
+    signal: "🟢 BUY",
+    health: 88,
+    growth: 85,
+    valuation: 78,
+    risk: 35,
+    profitability: 86,
+    cashflow: 87
+  }
+};

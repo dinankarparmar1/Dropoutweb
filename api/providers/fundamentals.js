e8e8f6fb05581a70082be3da1bc52f092d6b1c32
dinankarparmar1 <dinankarparmar12345@gmail.com>
2026-07-30 23:10:25 +0530

    Update fundamentals.js

--- api/providers/fundamentals.js
+++ api/providers/fundamentals.js
@@ -1,1 +1,41 @@
+// ==========================================
+// Dropout Fundamentals Provider
+// Version 1.0
+// ==========================================
 
+export async function getFundamentals(symbol){
+
+    try{
+
+        /*
+        Future connection point:
+
+        Here we will connect the
+        financial data provider.
+
+        It should return:
+
+        revenue
+        netIncome
+        debt
+        cashFlow
+        eps
+        balanceSheet
+        */
+
+        return null;
+
+
+    }
+    catch(error){
+
+        console.log(
+            "Fundamentals provider failed",
+            error
+        );
+
+        return null;
+
+    }
+
+}
